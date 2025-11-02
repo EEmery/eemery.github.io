492ba76a641ade6058448d7212786df9efe3e7ef
Eduardo Emery <emeryecs@gmail.com>
2025-11-02 19:46:15 -0300

    add atom and rss feeds by language

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,18 @@ from pelicanconf import *
 SITEURL = "https://robotsandcoffee.co"
 RELATIVE_URLS = False
 
+# --- Feed Configuration ---
+# ATOM feeds
 FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
+TRANSLATION_FEED_ATOM = "feeds/all-{lang}.atom.xml"
+
+# RSS feeds
+FEED_ALL_RSS = "feeds/all.rss.xml"
+CATEGORY_FEED_RSS = "feeds/{slug}.rss.xml"
+TRANSLATION_FEED_RSS = "feeds/all-{lang}.rss.xml"
+
+# Optional: Limit number of items in feeds
+FEED_MAX_ITEMS = 20
 
 DELETE_OUTPUT_DIRECTORY = True
